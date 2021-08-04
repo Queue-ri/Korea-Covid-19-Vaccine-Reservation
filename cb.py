806550dfe4e9b20d630fa88fc8e8d0bc7ea65477
Queue.ri <qriositylog@gmail.com>
2021-08-04 14:38:08 +0900

    feat : Add user name

--- cb.py
+++ cb.py
@@ -69,16 +69,16 @@ def check_user_info_loaded():
         if user_info['status'] == "NORMAL":
             print(f"사용자 정보를 불러오는데 성공했습니다. 사용자명:{user_info['name']}")
         elif user_info['status'] == "UNKNOWN":
-            print("상태를 알 수 없는 사용자입니다. 1339 또는 보건소에 문의해주세요.")
+            print("상태를 알 수 없는 사용자입니다. 1339 또는 보건소에 문의해주세요. 사용자명:{user_info['name']}")
             close()
         elif user_info['status'] == "REFUSED":
-            print("백신을 예약하고 방문하지 않은 사용자로 파악됩니다. 잔여백신 예약이 불가합니다.")
+            print("백신을 예약하고 방문하지 않은 사용자입니다. 잔여백신 예약이 불가합니다. 사용자명:{user_info['name']}")
             close()
         elif user_info['status'] == "ALREADY_RESERVED" or user_info['status'] == "ALREADY_VACCINATED":
-            print("이미 접종이 완료되었거나 예약이 완료된 사용자입니다.")
+            print("이미 접종이 완료되었거나 예약이 완료된 사용자입니다. 사용자명:{user_info['name']}")
             close()
         else:
-            print(f"알려지지 않은 상태 코드입니다. 상태코드:{user_info['status']}")
+            print(f"알려지지 않은 상태 코드입니다. 사용자명:{user_info['name']} 상태코드:{user_info['status']}")
             print("상태 코드 정보와 함께 Issues 생성 부탁드립니다.")
             close()
 
